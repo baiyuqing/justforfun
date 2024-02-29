a13c5c470547ad4b406066361d6425f7ec6b42ae
baiyuqing <baiyuq@gmail.com>
2024-02-29 20:11:18 +0800

    update

--- extension.js
+++ extension.js
@@ -5,29 +5,66 @@ const vscode = require('vscode');
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
-
+const JustForFunProperty = {
+  fontFamily: 0,
+  fontSize: 1,
+  fontAliasing: 2,
+};
 function updateFontFamily() {
   const isDark = vscode.window.activeColorTheme.kind === 2;
   const editorConfig = vscode.workspace.getConfiguration("editor");
   const preferredDarkFont =
-    editorConfig.get("preferredDarkFont") || "JetBrains Mono";
+    editorConfig.get("preferredDarkFont") || "JetBrains Mono,16,default";
   const preferredLightFont =
-    editorConfig.get("preferredLightFont") || "JetBrains Mono Medium";
-  editorConfig
-    .update(
+    editorConfig.get("preferredLightFont") ||
+    "JetBrains Mono Medium,16,default";
+
+  const darkProperties = String(preferredDarkFont).split(",");
+  const lightProperties = String(preferredLightFont).split(",");
+  const workbenchConfig = vscode.workspace.getConfiguration("workbench");
+
+  const updateConfigs = [];
+
+  updateConfigs.push(
+    editorConfig.update(
       "fontFamily",
-      isDark ? preferredDarkFont : preferredLightFont,
+      isDark
+        ? darkProperties[JustForFunProperty.fontFamily]
+        : lightProperties[JustForFunProperty.fontFamily],
       vscode.ConfigurationTarget.Global,
       true
     )
-    .then(
-      () => {
-        vscode.window.showInformationMessage("Update fontFamily");
-      },
-      (error) => {
-        vscode.window.showErrorMessage(`Fail to update fontFamily: ${error}`);
-      }
-    );
+  );
+
+  updateConfigs.push(
+    editorConfig.update(
+      "fontSize",
+      isDark
+        ? Number(darkProperties[JustForFunProperty.fontSize])
+        : Number(lightProperties[JustForFunProperty.fontSize]),
+      vscode.ConfigurationTarget.Global,
+      true
+    )
+  );
+
+  updateConfigs.push(
+    workbenchConfig.update(
+      "fontAliasing",
+      isDark
+        ? darkProperties[JustForFunProperty.fontAliasing]
+        : lightProperties[JustForFunProperty.fontAliasing],
+      vscode.ConfigurationTarget.Global,
+      true
+    )
+  );
+  Promise.all(updateConfigs).then(
+    () => {
+      vscode.window.showInformationMessage("Success to update justforfun");
+    },
+    (error) => {
+      vscode.window.showErrorMessage(`Fail to update justforfun: ${error}`);
+    }
+  );
 
   /*
   const workbenchConfig = vscode.workspace.getConfiguration("workbench");
